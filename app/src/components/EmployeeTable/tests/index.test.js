c056d161fe8d9f66ec9d84be80fafe3ff9238e07
Ryan Collins <rccproductions@mac.com>
2016-09-22 08:35:26 -0400

    Feat: finish testing for employee table and filter issue table

--- app/src/components/EmployeeTable/tests/index.test.js
+++ app/src/components/EmployeeTable/tests/index.test.js
@@ -1,11 +1,36 @@
 import EmployeeTable from '../index';
-
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
+import Box from 'grommet/components/Box';
+import Table from 'grommet/components/Table';
+import TableRow from 'grommet/components/TableRow';
 
 describe('<EmployeeTable />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  const employees = [
+    {
+      id: '57d02b4eafd9fad06655b484',
+      location: 'Carrsville',
+      numemployees: 166,
+    },
+    {
+      id: '57d02b4e737be284278d187d',
+      location: 'Frystown',
+      numemployees: 176,
+    },
+  ];
+  const wrapper = shallow(
+    <EmployeeTable
+      employees={employees}
+      onSelctItem={(e) => e}
+      selectedIndex={1}
+    />
+  );
+  it('should render with a surrounding Box', () => {
+    expect(wrapper.find(Box)).toExist();
+  });
+  it('should render a Table with TableRow', () => {
+    expect(wrapper.find(Table)).toExist();
+    expect(wrapper.find(TableRow)).toExist();
   });
 });
--- app/src/components/FilterIssueTable/tests/index.test.js
+++ app/src/components/FilterIssueTable/tests/index.test.js
@@ -1,11 +1,31 @@
 import FilterIssueTable from '../index';
-
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
+import Section from 'grommet/components/Section';
+import Menu from 'grommet/components/Menu';
+import Button from 'grommet/components/button';
+import Close from 'grommet/components/icons/base/Close';
+import Footer from 'grommet/components/Footer';
+
+const mockFn = (e) => e;
 
 describe('<FilterIssueTable />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  const wrapper = shallow(
+    <FilterIssueTable
+      employees={[]}
+      customers={[]}
+      onFilter={mockFn}
+      onApplyFilters={mockFn}
+      onClearFilter={mockFn}
+      isFiltering
+    />
+  );
+  it('should render with grommet components', () => {
+    expect(wrapper.find(Section)).toExist();
+    expect(wrapper.find(Menu)).toExist();
+    expect(wrapper.find(Button)).toExist();
+    expect(wrapper.find(Close)).toExist();
+    expect(wrapper.find(Footer)).toExist();
   });
 });
