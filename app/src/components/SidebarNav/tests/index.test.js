0516f6ec6154b29b6f5364a99f4e07683ebd1af3
Ryan Collins <rccproductions@mac.com>
2016-09-25 14:15:37 -0400

    Feat: update template data with better json

--- app/src/components/SidebarNav/tests/index.test.js
+++ app/src/components/SidebarNav/tests/index.test.js
@@ -1,11 +1,37 @@
 import SidebarNav from '../index';
-
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
+import Split from 'grommet/components/Split';
+import Sidebar from 'grommet/components/Sidebar';
+import Menu from 'grommet/components/Menu';
 
 describe('<SidebarNav />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  it('should have expected components when in default state.', () => {
+    const wrapper = shallow(
+      <SidebarNav navActive={false}>
+        <div>
+          Hello World
+        </div>
+      </SidebarNav>
+    );
+    expect(
+      wrapper.find(<Split />)
+    ).toExist();
+  });
+  it('should render as expected when active', () => {
+    const wrapper = shallow(
+      <SidebarNav navActive={false}>
+        <div>
+          Hello World
+        </div>
+      </SidebarNav>
+    );
+    expect(
+      wrapper.find(<Sidebar />)
+    ).toExist();
+    expect(
+      wrapper.find(<Menu />)
+    ).toExist();
   });
 });
