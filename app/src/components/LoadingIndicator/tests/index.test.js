80ad045b6f22950828d2b9dd66e1b5a4e9c1a8b5
Ryan Collins <rccproductions@mac.com>
2016-09-25 12:44:26 -0400

    Feat: pass all test, yay TDD

--- app/src/components/LoadingIndicator/tests/index.test.js
+++ app/src/components/LoadingIndicator/tests/index.test.js
@@ -2,12 +2,27 @@ import LoadingIndicator from '../index';
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
+import Spinning from 'grommet/components/icons/Spinning';
+import Box from 'grommet/components/Box';
+import Heading from 'grommet/components/Heading';
 
 describe('<LoadingIndicator />', () => {
   it('should render with an h2', () => {
     const component = shallow(<LoadingIndicator />);
     expect(
       component.find('h2')
+    ).toExist();
+  });
+  it('should render with expected components', () => {
+    const component = shallow(<LoadingIndicator isLoading />);
+    expect(
+      component.find(<Spinning />)
+    ).toExist();
+    expect(
+      component.find(<Box />)
+    ).toExist();
+    expect(
+      component.find(<Heading />)
     ).toExist();
   });
 });
