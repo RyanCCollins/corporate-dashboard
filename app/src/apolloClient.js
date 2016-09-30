a5c4e22a028778dc360f3f988cf53abfa3216a27
Ryan Collins <rccproductions@mac.com>
2016-09-30 11:51:42 -0400

    Feat: add production bundle with production api url

--- app/src/apolloClient.js
+++ app/src/apolloClient.js
@@ -2,12 +2,14 @@ import ApolloClient, {
   createNetworkInterface,
   addTypeName,
 } from 'apollo-client';
-
+const isProduction = process.env.NODE_ENV !== 'development';
 const productionUrl = 'https://corporate-dashboard-client.herokuapp.com/api';
 const testUrl = 'http://0.0.0.0:1338/api';
 
+const url = isProduction ? productionUrl : testUrl;
+
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface(testUrl),
+  networkInterface: createNetworkInterface(url),
   queryTransformer: addTypeName,
 });
 
